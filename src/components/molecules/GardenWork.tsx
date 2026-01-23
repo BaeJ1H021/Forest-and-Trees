2592dac443b06d4c6395f24f2fca4b39320aceb5
배지호 <blueapple99@naver.com>
2026-01-24 03:54:53 +0900

    fix: 안쓰는 함수 삭제

--- src/components/molecules/GardenWork.tsx
+++ src/components/molecules/GardenWork.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled, { css } from 'styled-components';
 import { useInViewOnce } from '../../hooks/useInViewOnce';
 
